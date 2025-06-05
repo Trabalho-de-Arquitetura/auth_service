29223aef0aecdf1fe3ba5a39559f30875f3a784d
Guilherme Chaves <guilherme.chaves@aipella.com>
2025-06-04 23:37:31 -0300

    fix: transforming-into-graphql-api

---
+++ src/main/java/com/authentication/entity/UserRole.java
@@ -0,0 +1,11 @@
+package com.authentication.entity;
+
+public enum UserRole {
+    ADMIN("admin"),
+    PROFESSOR("professor"),
+    STUDENT("student");
+
+    private final String role;
+    UserRole(String role) { this.role = role; }
+    public String getRole() { return role; }
+}
